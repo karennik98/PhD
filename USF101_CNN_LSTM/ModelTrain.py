c880768ae0011d1f9ddef4c8f397b257abdccc07
Karen Nikoghosyan <karen.nikoghosyan.98@gmail.com>
2023-03-13 12:10:46 -0700

    BFS/NC added

--- USF101_CNN_LSTM/ModelTrain.py
+++ USF101_CNN_LSTM/ModelTrain.py
@@ -13,7 +13,81 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import itertools
 import Utils
 
+def BFS(img1, img2):
+    # img1_orig = img1
+    # img2_orig = img2
+    # img1 = frames_list[i]
+    # img2 = frames_list[i+1]
+    img1 = cv2.normalize(img1, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    img2 = cv2.normalize(img2, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+    diff_mat = np.subtract(gray1, gray2)
+    # Step 5: Find out the sum of element of diff. matrix
+    sum_diff = np.sum(np.abs(diff_mat))
+    # Step 8: Mean -> mean2(S)
+    mean = np.mean(sum_diff)
+    # Step 9: Standard Deviation std2 (S)
+    std = np.std(sum_diff)
+    # Step 10: Find threshold
+    a = 1.5  # Constant
+    threshold = std + (mean * a)
+    # Step 11: if(S > threshold)
+    if sum_diff < threshold:
+        # Step 13: Write image J as the key frame
+        return 0 
+    else:
+        # Step 13: Write image J as the key frame
+        return 1
+
+
+def BFS_feature_extraction(video_path):
+    width=60
+    height=60
+    frames_list=[]
+    sequence_length=5
+    #Read the Video
+    video_reader = cv2.VideoCapture(video_path)
+    frame_count=int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+    #get the frame count
+    skip_flag = 0
+    for counter in range(frame_count):
+        #Read the current frame
+        if skip_flag == 5:
+            ret1, frame1 = video_reader.read()
+            if not ret1:
+                break;
+            ret2, frame2 = video_reader.read()
+            if not ret2:
+                break;
+            
+            #Resize the image
+            # frameNC = frame
+            # cv2.fastNlMeansDenoisingColored(frame, frameNC, 10, 10, 7, 21)
+
+            frame1=cv2.resize(frame1, (height, width))
+            frame1 = frame1/255
+            frame2=cv2.resize(frame2, (height, width))
+            frame2 = frame2/255
+
+            #Append to the frame
+            code = BFS(frame1, frame2)
+            if code == 0:
+                frames_list.append(frame1)
+            else:
+                frames_list.append(frame2)
+            skip_flag = 0
+        else:
+            skip_flag+=1
+    video_reader.release()
+
+    #Return the Frames List
+    print(video_path, " : ", len(frames_list))
+    return frames_list
+
+
+global_count = 0
 # Enable GPU memory growth to prevent TensorFlow from allocating all memory on the GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -48,7 +122,6 @@ def feature_extraction(video_path):
     frame_count=int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
     #Calculate the interval after which frames will be added to the list
     skip_interval = max(int(frame_count/sequence_length), 1)
-    #iterate through video frames
     for counter in range(sequence_length):
         #Set the current frame postion of the video
         video_reader.set(cv2.CAP_PROP_POS_FRAMES, counter * skip_interval)
@@ -57,6 +130,8 @@ def feature_extraction(video_path):
         if not ret:
             break;
         #Resize the image
+        # frameNC = frame
+        # cv2.fastNlMeansDenoisingColored(frame, frameNC, 10, 10, 7, 21)
         frame=cv2.resize(frame, (height, width))
         frame = frame/255
         #Append to the frame
@@ -73,23 +148,42 @@ def load_video(datasets):
     images=[]
     #Iterate through each foler corresponding to category
     for folder in datasets:
+        print(folder)
         for file in tqdm(os.listdir(folder)):
             #Get the path name for each video
             video_path = os.path.join(folder, file)
             #Extract the frames of the current video
-            frames_list = feature_extraction(video_path)
+            frames_list = BFS_feature_extraction(video_path)
+            # frames_list = feature_extraction(video_path)
+            # print(file, ": frames_list count: ", len(frames_list))
             images.append(frames_list)
             labels.append(label_index)
         label_index+=1
+    print("--------------------start--------------------")
+    print("len(images): ", len(images))
+    print("len(labels): ", len(labels))
+    print("--------------------end--------------------")
+
+    min_len = 1000000
+    for frames in images:
+        if len(frames) < min_len:
+            min_len = len(frames)
+    print("min_len: ", min_len)
+    for frames in images:
+        while len(frames) != min_len:
+            frames.pop()
+
     return np.array(images, dtype='float16'), np.array(labels, dtype='int8')
 
 #Due to memory allocation problem. I will select last 60 video folders for classification
-images, labels = load_video(path[41:])
+images, labels = load_video(path[96:])
 
 #Shapes
+print("Shapes")
 print(images.shape, pd.Series(labels).shape)
 
 #Train Test Split
+print("Test and Train")
 x_train, x_test, y_train, y_test=train_test_split(images, labels, test_size=0.06, random_state=10)
 print(x_train.shape, x_test.shape, np.array(y_train).shape, np.array(y_test).shape)
 
@@ -125,15 +219,15 @@ model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='accuracy')
 
 #Model training
-es = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, mode='min', restore_best_weights=True)
+# es = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, mode='min', restore_best_weights=True)
 
 # Train the model on GPU
 with tf.device('/GPU:0'):
-    history = model.fit(x_train, keras.utils.to_categorical(y_train), batch_size=32, epochs=50, validation_data=(x_test, keras.utils.to_categorical(y_test)), callbacks=[es])
+    history = model.fit(x_train, keras.utils.to_categorical(y_train), batch_size=Utils.batch_size, epochs=Utils.epochs, validation_data=(x_test, keras.utils.to_categorical(y_test)))
 
 #Plot the graph to check training and testing accuracy over the period of time
 plt.figure(figsize=(13,5))
-plt.title("Accuracy vs Epochs")
+plt.title("Accuracy vs Epochs") 
 plt.plot(history.history['accuracy'], label='Train Accuracy')
 plt.plot(history.history['val_accuracy'], label='Test Accuracy')
 plt.legend(loc='best')
--- USF101_CNN_LSTM/Utils.py
+++ USF101_CNN_LSTM/Utils.py
@@ -1,3 +1,5 @@
 anotations_path = "C:\\Users\\karen\\PhD\\UCF101_VGG16\\ucfTrainTestlist\\"
 data_path = "C:\\Users\\karen\\PhD\\UCF101_VGG16\\UCF-101\\"
-model_path = "C:\\Users\\karen\\PhD\\UCF101_VGG16\\Model\\CNN_Model.h5"
+model_path = "C:\\Users\\karen\\PhD\\UCF101_VGG16\\Model\\CNN_Model_BFS.h5"
+batch_size=64
+epochs=100
